27e9113fe27607c32398f86cb7bd75318c58adb0
Thomas Gak Deluen <tgdn45@gmail.com>
2020-11-28 17:47:13 +0100

    Some fixes to mobile border-bottom

--- components/dashboard/tab-selector.tsx
+++ components/dashboard/tab-selector.tsx
@@ -1,20 +1,40 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useContext } from "react";
 import cx from "classnames";
+import {
+  DashboardContext,
+  IDashboardContext,
+} from "providers/dashboard.provider";
 import { PrimaryButton } from "components/ui/buttons";
 import NewBet from "components/ui/new-bet";
+import BetList from "components/ui/bet-list";
+
+function Ongoing() {
+  const { allBets }: IDashboardContext = useContext(DashboardContext);
+  return <BetList paginatedBets={allBets} />;
+}
+
+function Awaiting() {
+  const { allBetsAwaiting }: IDashboardContext = useContext(DashboardContext);
+  return <BetList paginatedBets={allBetsAwaiting} />;
+}
+
+function Past() {
+  const { allBets }: IDashboardContext = useContext(DashboardContext);
+  return <BetList paginatedBets={allBets} />;
+}
 
 const tablist = {
   ongoing: {
     label: "Ongoing",
-    component: () => <>ongoing</>,
+    component: Ongoing,
   },
   awaiting: {
     label: "Awaiting",
-    component: () => <>awaiting</>,
+    component: Awaiting,
   },
   past: {
     label: "Past",
-    component: () => <>past</>,
+    component: Past,
   },
 };
 
@@ -22,12 +42,15 @@ function TabLabel({ children, isCurrent, setCurrent }) {
   return (
     <div
       onClick={setCurrent}
-      className={cx("cursor-pointer select-none text-lg px-3 py-1 ", {
-        "border-b-4": isCurrent,
-        "text-gray-300": !isCurrent,
-        "text-white": isCurrent,
-        "font-medium": isCurrent,
-      })}
+      className={cx(
+        "cursor-pointer select-none md:text-lg px-2 py-3 md:px-3 md:py-2 ",
+        {
+          "border-b-4": isCurrent,
+          "text-gray-300": !isCurrent,
+          "text-white": isCurrent,
+          "font-medium": isCurrent,
+        }
+      )}
     >
       {children}
     </div>
@@ -37,7 +60,7 @@ function TabLabel({ children, isCurrent, setCurrent }) {
 function Tabs({ children, setCurrent, currentTab }) {
   return (
     <div className="flex items-center p-1 border-b-4 border-gray-700">
-      <div className="flex -mb-2">
+      <div className="flex -mb-2 md:-mb-2">
         {Object.entries(tablist).map(([key, tab]) => (
           <TabLabel setCurrent={setCurrent(key)} isCurrent={currentTab === key}>
             {tab.label}
